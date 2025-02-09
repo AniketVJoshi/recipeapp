dd463a4c22fc37d6743f67175912044f16442796
Aniket Joshi <joshi.aniket.v@gmail.com>
2025-02-10 00:32:18 +0530

    Fixed Linting issues

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,9 +2,11 @@
 Calculator Functions
 """
 
+
 def add(x, y):
     """Add x, y and return result"""
     return x + y
+
 
 def subtract(x, y):
     """Subtract x from y and return result"""
--- app/app/tests.py
+++ app/app/tests.py
@@ -4,6 +4,7 @@ Sample Test
 from django.test import SimpleTestCase
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
     """Test the Calc Module."""
 
